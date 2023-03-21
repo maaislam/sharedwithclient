52577cb27f2f742e3ef901d071fcec4e3b787d6e
Arafat Islam <maaislam@outlook.com>
2023-03-21 15:12:11 +0600

    Add files via upload

---
+++ triggers.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview The triggers file contains all activation conditions for the experiment.
+ * This is the first file to be evaluated.
+ */
+import activate from './lib/experiment';
+import { pollerLite } from '../../../../lib/uc-lib';
+
+const ieChecks = /MSIE|Trident|Edge\/(12|13|14|15|16|17|18)/.test(window.navigator.userAgent);
+console.log('test');
+if (!ieChecks) {
+  pollerLite(['body'], activate);
+}
